c7b7a0455b20db4fc390f3de7f112295db2d1087
mcortinas <marc.cortinas@gmail.com>
2019-10-12 20:45:29 +0200

    Disabled all, and just enabled groovy from https://learning.oreilly.com/videos/devops-masterclass-ci-cd/9781789953626/9781789953626-video3_7

--- jobs/dslLoop.groovy
+++ jobs/dslLoop.groovy
@@ -1,6 +1,6 @@
 def gitUrl="https://github.com/quidryan/aws-sdk-test.git"
 for(i in 0..10){
-  job("DSL-Tutorial-1-Test${i}"){
+  job("dsl/DSL-Tutorial-1-Test${i}"){
     scm{
       git(giturl)
     }
