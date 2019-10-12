3122d01f83bf250cb1146189d029e8365d96c2dc
mcortinas <marc.cortinas@gmail.com>
2019-10-12 20:57:53 +0200

    added branches and fix loop

---
+++ jobs/dslBranches.groovy
@@ -0,0 +1,8 @@
+def branches = ['master','feature']
+branches.each { branch ->
+  job('jenkins-${branch}'){
+    scm{
+      github('jenkins/jendsl',branch)
+    }
+  }
+}
--- jobs/dslLoop.groovy
+++ jobs/dslLoop.groovy
@@ -2,7 +2,7 @@ def gitUrl="https://github.com/quidryan/aws-sdk-test.git"
 for(i in 0..10){
   job("DSL-Tutorial-1-Test${i}"){
     scm{
-      git(giturl)
+      git(gitUrl)
     }
     steps{
       maven("test -Dtest.suite=${i}")
